9b20bc4fd6b976bc4ce846bca7bed65b921b0739
edgar <xiexiaodong2015@gmail.com>
2017-11-26 23:18:22 +0800

    modified

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ monkey.patch_all()
 app = create_app('default')
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(host='0.0.0.0', debug=True)
+    # http_server = WSGIServer(('', 5000), app)
+    # http_server.serve_forever()
