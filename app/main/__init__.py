78ad3eea11ad8c081da9829e1fbe888790026830
Edgar <doforce@126.com>
2018-11-16 23:18:06 +0800

    delete flask

--- app/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-import asyncio
-
-main = Blueprint('main', __name__)
-
-from . import views
--- application.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from app import create_app
-
-app = create_app('default')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
